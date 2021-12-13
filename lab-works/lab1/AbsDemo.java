1c7a41e15a8cfed20004d46bd9f2b1e42d9281eb
Prajwalgautam <60287104+Prajwalgautam@users.noreply.github.com>
2021-12-13 21:50:51 +0545

    Deleted because of some file management
    Just to manage some file and their folders

--- lab-works/lab1/AbsDemo.java
+++
@@ -1,9 +0,0 @@
-class AbsDemo extends AbstractClass{
-	public void help(){
-	System.out.println("Abclass methode overide....");
-	}
-	public static void main(){
-	AbstractClass abClass = new AbsDemo();
-	
-	}
-}
