beb553077b6f0daf78764a7bed1f32198e40141e
Prajwalgautam <prajwalgautam007@gmail.com>
2021-12-13 21:51:56 +0545

    1st lab of java Subbimited dec 13 2021

--- lab-works/lab1/Abstract/AbsDemo.java
+++ lab-works/lab1/Abstract/AbsDemo.java
@@ -2,8 +2,8 @@ class AbsDemo extends AbstractClass{
 	public void help(){
 	System.out.println("Abclass methode overide....");
 	}
-	public static void main(){
-	AbstractClass abClass = new AbsDemo();
-	
+	public static void main(String[] args) {
+		AbstractClass abClass = new AbsDemo();
+		abClass.help();
 	}
 }
--- lab-works/lab1/AbstractClass.java
+++
@@ -1,3 +0,0 @@
-class AbstractClass{
-	abstract void help();
-}
