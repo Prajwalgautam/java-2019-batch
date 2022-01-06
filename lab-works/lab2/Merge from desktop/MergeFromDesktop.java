0efcbd1390b4906860cc8ce61dee6a7c5234af55
Prajwalgautam <prajwalgautam007@gmail.com>
2022-01-06 16:02:55 +0545

    Changes made on some codes in jan 6

--- lab-works/lab2/Merge from desktop/MergeFromDesktop.java
+++ lab-works/lab2/Merge from desktop/MergeFromDesktop.java
@@ -3,22 +3,13 @@ class MergeFromDesktop {
  
     public static void main(String[] args) throws IOException
     {
-        File dir = new File("C:\\Asus\\Desktop");
+        File dir = new File("C:\\Users\\ASUS\\Desktop");
         PrintWriter pw = new PrintWriter("output.txt");
         String[] fileNames = dir.list();
         for (String fileName : fileNames) {
-            System.out.println("Reading from " + fileName);
-            File f = new File(dir, fileName);
-            BufferedReader br = new BufferedReader(new FileReader(f));
-            pw.println("Contents of file " + fileName);
-            String line = br.readLine();
-            while (line != null) {
-                pw.println(line);
-                line = br.readLine();
-            }
+            pw.println(fileName);
             pw.flush();
         }
-        System.out.println("Reading from all files" +
-        " in directory " + dir.getName() + " Completed");
+        System.out.println("Reading from all files"+" in directory " + dir.getName() + " Completed");
     }
 }
--- lab-works/lab2/Own Exception Class/InvalidAgeException.java
+++ lab-works/lab2/Own Exception Class/InvalidAgeException.java
@@ -1,13 +1,18 @@
-public class InvalidAgeException extends Exception
+public class InvalidAgeException
 {
-    private double value;
 
-    public InvalidAgeException(double value, String message)
+    public void validate(int age)
     {
-        super(message);
-        this.value = value;
-        if(value < 1 || value > 100){
-            message = "Age is not to be lessthan 1 nor exceeds 100";
+        if(age < 1 || age > 100){
+            throw new ArithmeticException("Invalid age : Age is not to be less than 1 nor exceeds 100");
+        }
+    }
+    public static void main(String[] args) {
+        InvalidAgeException exception = new InvalidAgeException();
+        try{
+            exception.validate(180);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
